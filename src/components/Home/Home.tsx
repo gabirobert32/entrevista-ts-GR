b745656c983dedd2a83238fcb2bbc9a14750830d
Nicolas Lucero <nicolas.lucero@redb.ee>
2022-09-21 10:29:43 -0300

    correction

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,55 +1,98 @@
-import axios from 'axios'
-import { useState } from 'react'
-import styled from 'styled-components'
-import "./Home.css";
+import axios from "axios";
+import { ChangeEvent, useState } from "react";
+import styled from "styled-components";
+import { PokemonWrapper } from "../../models/Pokemon";
 
 const Padding = styled.div`
   padding: 2em;
-`
+`;
+const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding: 2em;
+`;
+
+const Name = styled.div`
+  padding: 2em;
+`;
+const Button = styled.button`
+  padding: 10px;
+  border-radius: 10px;
+  margin: 10px;
+  border: 1px solid #80808052;
+  color: black;
+  background: #6aa1ff;
+  cursor: pointer;
+  transition: 0.4s;
+  &:hover {
+    background: blue;
+    color: white;
+  }
+`;
+const Select = styled.select`
+  padding: 10px;
+  border: 1px solid #80808052;
+  border-radius: 10px;
+  margin: 10px;
+  color: gray;
+`;
+
+const Card = styled.div`
+  min-width: 30%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #8080803b;
+  transition: 0.4s;
+  margin: 10px;
+  border-radius: 10px;
+  &:hover {
+    box-shadow: 3px 2px 5px 0 #8080803b;
+  }
+`;
 
 const Home = () => {
-  const [pokemons, setPokemons] = useState([])
+  const [pokemonType, setPokemonType] = useState<string>("");
+  const [pokemon, setPokemon] = useState<PokemonWrapper[]>();
 
-  const [pokemon, setPokemon] = useState([])
+  const URL = `https://pokeapi.co/api/v2/type/${pokemonType}`;
 
-  const URL = `https://pokeapi.co/api/v2/type/${pokemons}`
+  const handleChange = ({ target }: ChangeEvent<HTMLSelectElement>) => {
+    setPokemonType(target.value);
+  };
 
-  const handleChange = (e: any) => {
-    setPokemons(e.target.value)
-  }
-
-  const getData = (URL: string) => {
-    axios
-      .get(URL)
-      .then((res) => {
-        setPokemon(res.data.pokemon)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
+  const getData = async () => {
+    try {
+      const { data } = await axios.get(URL);
+      setPokemon(data.pokemon);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Padding>
-      <select placeholder="Nombre de pokemon" onChange={handleChange}>
+      <Select placeholder="Nombre de pokemon" onChange={handleChange}>
         <option value="">Seleccionar</option>
         <option value="normal">Normal</option>
         <option value="electric">Electic</option>
         <option value="water">Water</option>
         <option value="fire">Fire</option>
         <option value="ghost">Ghost</option>
-      </select>
-      <button onClick={() => getData(URL)}>Buscar</button>
+      </Select>
+      <Button onClick={getData}>Buscar</Button>
 
       {!!pokemon && (
-        <div className="pokeContainer">
-          {pokemon.map((p: any, index: number) => (
-             <div className="pokeCard" key={index}>{p.pokemon.name}</div>
+        <FlexContainer>
+          {pokemon.map(({ pokemon }: PokemonWrapper) => (
+            <Card key={`pokemon-${pokemon.name}`}>
+              <Name>{pokemon.name}</Name>
+            </Card>
           ))}
-        </div>
+        </FlexContainer>
       )}
     </Padding>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
